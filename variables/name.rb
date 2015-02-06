9c23a14546226e07115d0e3a4d845a66800af499
Reltre <tglippert@gmail.com>
2015-02-05 23:23:50 -0800

    exercise answers to variables section

---
+++ variables/name.rb
@@ -0,0 +1,16 @@
+#Number 1
+=begin
+puts "Please type in your name"
+name = gets.chomp
+puts "Hello #{name}!"
+=end
+
+#Number 3
+#10.times {puts name}
+
+#Number 4
+puts "Please type in your first name"
+f_name = gets.chomp
+puts "Please type in your last name"
+l_name = gets.chomp
+puts "#{f_name} #{l_name}"
